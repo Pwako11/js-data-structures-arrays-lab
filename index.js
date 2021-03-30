ebc52016b06f822d056fe2adb65056ca29771fc9
Pwako11 <akolo@hotmail.com>
2021-03-30 14:23:47 -0400

    commit tree cce3ef01da7ebfac25a0de01491b5dc22afa4a19

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Write your solution here!
+
+let drivers = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendDriver(name) {
+    drivers.push(name)
+};
+
+function destructivelyPrependDriver(name){
+    drivers.unshift(name)
+};
+
+function destructivelyRemoveLastDriver(name) {
+    drivers.pop(name)
+};
+
+function destructivelyRemoveFirstDriver(name) {
+    drivers.shift(name)
+};
+
+function appendDriver(name) {
+
+    const newDriver = [...drivers.slice(0, 3), name];
+    
+    return newDriver
+};
+
+function prependDriver(name) {
+    const newDriver = [...drivers.slice(0, 0), name, ...drivers.slice(0)];
+    return newDriver
+};
+
+
+function removeLastDriver() {
+    let newDriver = drivers.slice( 0, drivers.length -1);
+    return newDriver
+};
+
+function removeFirstDriver() {
+    let newDriver = drivers.slice(1);
+    return newDriver
+};
